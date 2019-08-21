d71ab08714265f87833fe95dd8956b7357fc4263
Shant Khayalian <noiroxc@gmail.com>
2019-08-21 07:22:19 -0700

    commit

---
+++ src/main/java/com/ithome/web/start/UserSection/OurTeam.java
@@ -0,0 +1,106 @@
+package com.ithome.web.start.UserSection;
+
+import com.ithome.web.start.Beans.Employee;
+import com.ithome.web.start.DaoController.EmployeeDao;
+import com.ithome.web.start.Helpers.LanguageHelper;
+import com.ithome.web.start.Helpers.PageNameHelper;
+import com.ithome.web.start.Helpers.SessionChecker;
+import com.ithome.web.start.Localization.CheckLanguageAndCurrency;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/OurTeam")
+public class OurTeam extends HttpServlet {
+    private CheckLanguageAndCurrency checkLanguageAndCurrency = new CheckLanguageAndCurrency();
+    private String language = null;
+    private String Pagelanguage = null;
+    private String pageName = null;
+    private String pageLanguageName = null;
+    private SessionChecker checker = new SessionChecker();
+    private String sessionId = null;
+    private LanguageHelper languageHelper = new LanguageHelper();
+    private PageNameHelper pageNameHelper = new PageNameHelper();
+    private List<Employee> employeeList = new ArrayList<>();
+    private EmployeeDao employeeDao = new EmployeeDao();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ourTeam(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ourTeam(request,response);
+    }
+
+    private void ourTeam(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        sessionControlling(request,response);
+        getLanguagesFromPage(request);
+        getPageName(request);
+        getPageLanguage(language);
+        getPageLabguageName(language);
+        getEmployeeList();
+        setRequestedToPage(request);
+        gotoPage(request,response);
+    }
+
+    private void gotoPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/OurTeam.jsp").forward(request,response);
+    }
+
+    private void setRequestedToPage(HttpServletRequest request) {
+        request.setAttribute("Pagelanguage", Pagelanguage);
+        request.setAttribute("PageName", pageName);
+        request.setAttribute("pageLanguageName", pageLanguageName);
+        request.setAttribute("sessionId", sessionId);
+        request.setAttribute("employeeList", employeeList);
+    }
+
+    private void getEmployeeList() {
+        employeeList = employeeDao.getAllEmployee();
+    }
+
+    private void getPageLanguage(String language) {
+        Pagelanguage = checkLanguageAndCurrency.checkLanguage(language);
+    }
+
+    private void getPageLabguageName(String language) {
+        pageLanguageName = checkLanguageAndCurrency.checkLanguageName(language);
+    }
+
+
+    private void sessionControlling(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false);
+        getUserSession(session, request, response );
+    }
+
+    private void getUserSession(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+        if(checker.checkSessionUser(request, response)) {
+            sessionId = checker.requestSessionofUser(session);
+        }else{
+            sessionId = session.getId();
+        }
+    }
+
+    private String getLanguagesFromPage(HttpServletRequest request) {
+        if (request.getParameter("Pagelanguage") == null) {
+            language = languageHelper.Pagelanguage(request, Pagelanguage);
+        } else {
+            language = languageHelper.Pagelanguage(request, request.getParameter("Pagelanguage"));
+        }
+        return language;
+    }
+
+    private void getPageName(HttpServletRequest request) {
+        pageName = pageNameHelper.pageName(request);
+        System.out.println(pageName);
+    }
+}
+
